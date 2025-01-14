61f301c344499987bb01da523127cd9eee9db22d
Limbend <termir988@gmail.com>
2025-01-14 14:18:05 +0300

    feat: update python and poetry dependencies

--- backend/docker/dev.entrypoint.sh
+++ backend/docker/dev.entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # You must use line breaks in LF format!
-# Otherwise the container sends non-obvious errors. As if such a file does not exist. 
+# Otherwise the container sends non-obvious errors. As if such a file does not exist.
 
 sleep 2
 
+echo "dev.entrypoint.sh -- Start alembic upgrade"
 cd /app
 alembic upgrade head
 
+echo "dev.entrypoint.sh -- Run fill_empty_db.py"
 cd tests
-
 python fill_empty_db.py
 
+echo "dev.entrypoint.sh -- Start app"
 cd ../api
-
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
