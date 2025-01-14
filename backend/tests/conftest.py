cb3e12f2f872b8d4070bc2b82c04f1d70d87c8d6
Limbend <termir988@gmail.com>
2025-01-14 12:43:07 +0300

    fix: pytest
    - Current tests are wrapped into a common class
    - Added environment validation
    - Added execution order and dependencies

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -12,31 +12,7 @@ sys.path.append(os.path.join(sys.path[0], ".."))
 sys.path.append(os.path.join(sys.path[0], "../api"))
 
 from main import app
-
-
-# import asyncio
-# from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-# from sqlalchemy.pool import NullPool
-
-# from api.config import settings
-# from api.repository import new_se ssion
-
-
-# engine_test = create_async_engine(
-#     settings.db.connection_url, poolclass=NullPool, echo=False
-# )
-# override_new_session = async_sessionmaker(engine_test, expire_on_commit=False)
-# app.dependency_overrides[new_session] = override_new_session
-
-
-# metadata.bind = engine_test
-# @pytest.fixture(autouse=True, scope="session")
-# async def prepare_database():
-#     async with engine_test.begin() as conn:
-#         await conn.run_sync(metadata.create_all)
-#     yield
-#     async with engine_test.begin() as conn:
-#         await conn.run_sync(metadata.drop_all)
+from config import settings
 
 
 # https://pytest-asyncio.readthedocs.io/en/latest/how-to-guides/run_session_tests_in_same_loop.html
@@ -52,6 +28,8 @@ client = TestClient(app)
 
 @pytest.fixture(scope="session")
 async def ac() -> AsyncGenerator[AsyncClient, None]:
+    assert settings.launch_mode == "test"
+
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as async_test_client:
--- backend/tests/test_product_api.py
+++ backend/tests/test_product_api.py
@@ -1,74 +1,89 @@
 from httpx import AsyncClient
 from datetime import date
-
-base_url = "/products"
-product_id: int
-guarantee_end_date: date
+import pytest
 
 
-async def test_add_product(ac: AsyncClient):
-    response = await ac.post(
-        base_url,
-        json={
-            "name": "Xiaomi Notebook",
-            "model": "Air 13.3",
-            "price": 49899.30,
-            "is_purchased": True,
-            "buy_date": "2024-10-12",
-            "guarantee": 12,
-            "product_link": "https://www.mi.com",
-            "priority": 1,
-        },
-    )
-
-    assert response.status_code == 200
-    response = response.json()
-    assert response["ok"] == True
-    assert "product_id" in response["content"].keys()
-    assert "guarantee_end_date" in response["content"]["auto_generated_fields"].keys()
-
-    global product_id, guarantee_end_date
-    product_id = response["content"]["product_id"]
-    guarantee_end_date = response["content"]["auto_generated_fields"][
-        "guarantee_end_date"
-    ]
+@pytest.fixture(scope="class")
+async def product_data():
+    data = {"product_id": None, "guarantee_end_date": None}
+    yield data
 
 
-async def test_get_product(ac: AsyncClient):
-    chunk_size = 25
+class TestProductApi:
+    base_url = "/products"
 
-    response = await ac.get(
-        base_url,
-        params={"by": chunk_size, "chunk": 0, "field": "id", "desc": False},
-    )
+    @pytest.mark.dependency(name="add", scope="session")
+    async def test_add_product(self, ac: AsyncClient, product_data):
+        response = await ac.post(
+            self.base_url,
+            json={
+                "name": "Xiaomi Notebook",
+                "model": "Air 13.3",
+                "price": 49899.30,
+                "is_purchased": True,
+                "buy_date": "2024-10-12",
+                "guarantee": 12,
+                "product_link": "https://www.mi.com",
+                "priority": 1,
+            },
+        )
 
-    assert response.status_code == 200
-    response = response.json()
-    assert response["ok"] == True
-    assert len(response["content"]) <= response["total_count"]
-    assert len(response["content"]) <= chunk_size
+        assert response.status_code == 200
+        response = response.json()
+        assert response["ok"] == True
+        assert "product_id" in response["content"].keys()
+        assert (
+            "guarantee_end_date" in response["content"]["auto_generated_fields"].keys()
+        )
 
+        product_data["product_id"] = response["content"]["product_id"]
+        product_data["guarantee_end_date"] = response["content"][
+            "auto_generated_fields"
+        ]["guarantee_end_date"]
 
-async def test_update_product(ac: AsyncClient):
-    response = await ac.patch(
-        base_url,
-        json={"id": product_id, "guarantee": 2, "model": "Air 14.4"},
-    )
+    @pytest.mark.dependency(depends=["add"], name="get", scope="session")
+    async def test_get_product(self, ac: AsyncClient):
+        chunk_size = 25
 
-    assert response.status_code == 200
-    response = response.json()
-    assert response["ok"] == True
-    assert response["content"]["name"] == "Xiaomi Notebook"
-    assert response["content"]["guarantee"] == 2
-    assert guarantee_end_date != response["content"]["guarantee_end_date"]
+        response = await ac.get(
+            self.base_url,
+            params={"by": chunk_size, "chunk": 0, "field": "id", "desc": False},
+        )
 
+        assert response.status_code == 200
+        response = response.json()
+        assert response["ok"] == True
+        assert len(response["content"]) <= response["total_count"]
+        assert len(response["content"]) <= chunk_size
 
-async def test_delete_product(ac: AsyncClient):
-    response = await ac.delete(
-        base_url,
-        params={"product_id": product_id},
-    )
+    @pytest.mark.dependency(depends=["add"], name="update", scope="session")
+    async def test_update_product(self, ac: AsyncClient, product_data):
+        response = await ac.patch(
+            self.base_url,
+            json={
+                "id": product_data["product_id"],
+                "guarantee": 2,
+                "model": "Air 14.4",
+            },
+        )
 
-    assert response.status_code == 200
-    response = response.json()
-    assert response["ok"] == True
+        assert response.status_code == 200
+        response = response.json()
+        assert response["ok"] == True
+        assert response["content"]["name"] == "Xiaomi Notebook"
+        assert response["content"]["guarantee"] == 2
+        assert (
+            product_data["guarantee_end_date"]
+            != response["content"]["guarantee_end_date"]
+        )
+
+    @pytest.mark.dependency(depends=["add", "get", "update"], scope="session")
+    async def test_delete_product(self, ac: AsyncClient, product_data):
+        response = await ac.delete(
+            self.base_url,
+            params={"product_id": product_data["product_id"]},
+        )
+
+        assert response.status_code == 200
+        response = response.json()
+        assert response["ok"] == True
